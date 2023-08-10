6aecea59c4c8eb7503c4ade6358fea32aa6543db
ARUM PUSPA KHINATHI <arum19puspakhi@gmail.com>
2023-08-10 20:55:47 +0700

    update 1

--- src/test/java/starter/Utils/Constants.java
+++ src/test/java/starter/Utils/Constants.java
@@ -1,2 +1,10 @@
-package starter.Utils;public class Constants {
+package starter.Utils;
+
+public class Constants {
+    private static final String BASE_URL = "https://cookit.altapro.online";
+    private static final String DIR = System.getProperty("user.dir");
+    public static final String BEARER_TOKEN = "";
+    public static String JSON_SCHEMA_DIR = DIR + "/src/test/resources/JSON_Schema";
+    public static String ASSETS_DIR = DIR + "/src/test/resources/assets";
+
 }
